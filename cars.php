3a98a8ef2e6b1a105cc6dcda113c99ca49192886
3041TAN-10 <3041TAN-10@boronkay.local>
2023-11-27 10:11:26 +0100

    modified

--- cars.php
+++ cars.php
@@ -1,37 +1,56 @@
 <?php
 
-$fileName = "car-db.csv";
-$csvdata = getCsvData($fileName);
+//require_once('csv-tools.php');
+ini_set('memory_limit','560M');
+$FileName  = "car-db.csv";
+$csvData = getCsvData($FileName);
 $result = [];
-$arr = array ('first' => 'a' , 'second' => 'b');
-$key = array_search('a'. $arr);
+$maker = [];
+$makers = [];
 $header = $csvData[0];
-$keyMaker = array_search('make', $header);
-$keyModel = array_search('model', $header);
+$idxMaker = array_search ('make', $header);
+$idxModel = array_search ('model', $header);
 
 
-function getCsvData($fileName, $withHeader = true) {
-if(!file_exists($fileName)) {
-    echo "$fileName nem található";
+function getCsvData($FileName){
+ 
+    if (!file_exists($FileName)) {
+        echo "$FileName nem található. ";
+        return false;
+    }
+ 
+    
+        $csvFile = fopen($FileName, 'r');
+        $lines = [];
+        while (! feof($csvFile)) {
+            $line = fgetcsv($csvFile);
+            $lines[] = $line;
+        }
+        fclose($csvFile);
+        return $lines;
+    
+    }
+
+ 
+if (empty($csvData)) {
+    echo "Nincs Adat!";
     return false;
 }
-
-    $csvFile = fopen($fileName, 'r');
-    $header = fgetcsv($csvFile);
-    $lines = [];
-    if ($withHeader) {
-        $lines[] = $header;
+$maker = "";
+$model = "";
+$isHeader = true;
+    foreach ($csvData as $idx => $line) {
+        if (!is_array($line)) {
+            continue;
+        }
+        if ($maker != $line[$idxMaker]){
+            $maker = $line[$idxMaker];
+            $makers[] = $maker;
+        }
+        if ($model != $line[$idxModel]){
+            $model = $line[$idxModel];
+            $result[$maker][] = $model;
+        }
     }
-    else {
-        $lines = [];
-    }
-    while (! feof($csvFile)) {
-        $line = fgetcsv($csvFile);
-        $lines[] = $line;
-    }
-    fclose($csvFile);
-
-    return $lines;
-}
-$csvData = getCsvData($fileName);
-?>
+    //print_r($result);
+    print_r($makers);
